d6823592ab4f2a0e59264a0db42491c4e06bef69
ShooterArk <smallassasin@gmail.com>
2024-09-13 23:42:55 +0500

    added context and providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import MiniApp from './MiniApp';
+import { AppContext } from './contexts/AppContext'
+import { SettingsContext } from './contexts/SettingsContext'
+import { UserContext } from './contexts/UserContext'
+import { AppStore } from './stores/AppStore'
+import { SettingsStore } from './stores/SettingsStore'
+import { UserStore } from './stores/UserStore'
+
+const userStore = new UserStore()
+const appStore = new AppStore()
+const settingsStore = new SettingsStore(appStore)
 
 function App() {
   const [isWidgetOpen, setIsWidgetOpen] = useState(false);
@@ -10,12 +20,18 @@ function App() {
   };
 
   return (
-    <div>
-      <button onClick={handleButtonClick} style={buttonStyle}>
-        Open Widget
-      </button>
-      {isWidgetOpen && ReactDOM.createPortal(<MiniApp closeWidget={handleButtonClick} />, document.body)}
-    </div>
+    <UserContext.Provider value={userStore}>
+      <AppContext.Provider value={appStore}>
+        <SettingsContext.Provider value={settingsStore}>
+          <div>
+            <button onClick={handleButtonClick} style={buttonStyle}>
+              Open Widget
+            </button>
+            {isWidgetOpen && ReactDOM.createPortal(<MiniApp closeWidget={handleButtonClick} />, document.body)}
+          </div>
+        </SettingsContext.Provider>
+      </AppContext.Provider>
+    </UserContext.Provider>
   );
 }
 
